95e12b11dac3eaa9e9d57561b6f90a36ad2568ec
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-03-25 01:52:58 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -10,7 +10,7 @@ sed -i 's/OpenWrt/Bin AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/de
 svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/brook
 svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
 svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/tcping
-svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-dockerman
+svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/bin20088/luci-theme-argon-mc.git package/luci-theme-argon-mc
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
