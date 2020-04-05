be73da3fd00bbd669c4dfa84e27ba9dc30449e25
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-04-05 23:41:41 +0800

    Update n1稳定版.sh

--- n1稳定版.sh
+++ n1稳定版.sh
@@ -7,7 +7,7 @@ sed -i 's/OpenWrt/Bin AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/de
 #移除不用软件包    
 #rm -rf package/lean/luci-app-dockerman
 #添加额外软件包
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 #添加神秘代码
 svn co https://github.com/bin20088/lede/trunk/files
