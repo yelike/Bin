4be087f7944014f30eb45ec3115e45a834729a55
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-04-16 16:30:27 +0800

    Update n1稳定版.sh

--- n1稳定版.sh
+++ n1稳定版.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 # Add kernel build user
-#修改版本号
+# 修改版本号
 sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-#移除不用软件包    
-##rm -rf package/lean/luci-app-dockerman
-#添加额外软件包
-##git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# 移除不用软件包    
+# rm -rf package/lean/luci-app-dockerman
+# 添加额外软件包
+# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 # git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 #./scripts/feeds update -a
 #./scripts/feeds install -a
