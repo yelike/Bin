2c8357a6366363c0dd735c937c012c3945d6ab5f
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-04-16 23:17:43 +0800

    Update n1稳定版.sh

--- n1稳定版.sh
+++ n1稳定版.sh
@@ -5,9 +5,15 @@
 # 修改版本号
 sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 # 移除不用软件包    
-# rm -rf package/lean/luci-app-dockerman
+rm -rf package/lean/luci-app-dockerman
 # 添加额外软件包
-# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koolddns
+git clone https://github.com/bin20088/luci-theme-butongwifi.git package/luci-theme-butongwifi
+git clone https://github.com/Repobor/luci-app-koolproxyR.git package/luci-app-koolproxyR
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+chmod 0755 package/luci-app-koolddns/root/etc/init.d/koolddns
+chmod 0755 package/luci-app-koolddns/root/usr/share/koolddns/aliddns
+./scripts/feeds update -a
+./scripts/feeds install -a
