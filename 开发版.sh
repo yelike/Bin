50ac8f0e7c4031b02113e5d5a0c8a224e0fc9c15
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-05-31 21:24:21 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -3,8 +3,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #修改版本号
 sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-#移除不用软件包    
-rm -rf package/lean/luci-app-dockerman
+
 #添加额外软件包
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 svn co https://github.com/bin20088/openwrt-package/trunk/package/brook package/brook
