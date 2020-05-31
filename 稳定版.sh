8e6c30a1222d904ac29832c4c6f230634b743315
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-05-31 21:24:58 +0800

    Update 稳定版.sh

--- 稳定版.sh
+++ 稳定版.sh
@@ -3,8 +3,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #修改版本号
 sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-#移除不用软件包    
-rm -rf package/lean/luci-app-dockerman
+
 #添加额外软件包
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
