ee53891492d56d60bc674ebd1ccd33346369ead5
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-06-21 18:20:17 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -5,21 +5,22 @@
 sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 
 #添加额外软件包
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-svn co https://github.com/bin20088/openwrt-package/trunk/package/brook package/brook
-svn co https://github.com/bin20088/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
-svn co https://github.com/bin20088/openwrt-package/trunk/package/tcping package/tcping
-svn co https://github.com/bin20088/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
-git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-git clone https://github.com/bin20088/luci-theme-argon-mc.git package/luci-theme-argon-mc
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/bin20088/luci-theme-butongwifi.git package/luci-theme-butongwifi
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
-git clone https://github.com/Repobor/luci-app-koolproxyR.git package/luci-app-koolproxyR
-git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koolddns
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
+svn co https://github.com/bin20088/openwrt-package/trunk/package/brook package/openwrt-packages/brook
+svn co https://github.com/bin20088/openwrt-package/trunk/package/chinadns-ng package/openwrt-packages/chinadns-ng
+svn co https://github.com/bin20088/openwrt-package/trunk/package/tcping package/openwrt-packages/tcping
+svn co https://github.com/bin20088/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
+git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
+git clone https://github.com/bin20088/luci-theme-argon-mc.git package/openwrt-packages/luci-theme-argon-mc
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/openwrt-packages/luci-theme-opentomcat
+git clone https://github.com/bin20088/luci-theme-butongwifi.git package/openwrt-packages/luci-theme-butongwifi
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/openwrt-packages/luci-theme-atmaterial
+git clone https://github.com/Repobor/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
+git clone https://github.com/bin20088/luci-app-koolddns.git package/openwrt-packages/luci-app-koolddns
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/openwrt-packages/luci-app-ssr-plus
+svn co https://github.com/bin20088/OpenClash/trunk/luci-app-openclash package/openwrt-packages/luci-app-openclash
 #赋予koolddns权限
-chmod 0755 package/luci-app-koolddns/root/etc/init.d/koolddns
-chmod 0755 package/luci-app-koolddns/root/usr/share/koolddns/aliddns
+chmod 0755 package/openwrt-packages/luci-app-koolddns/root/etc/init.d/koolddns
+chmod 0755 package/openwrt-packages/luci-app-koolddns/root/usr/share/koolddns/aliddns
 ./scripts/feeds update -a
 ./scripts/feeds install -a
