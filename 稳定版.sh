07b6922332aecd3c587df02484cf14d277c37824
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-08-11 09:24:54 +0800

    Update 稳定版.sh

--- 稳定版.sh
+++ 稳定版.sh
@@ -10,7 +10,7 @@ git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packa
 git clone https://github.com/bin20088/luci-app-koolddns.git package/openwrt-packages/luci-app-koolddns
 git clone https://github.com/bin20088/luci-theme-butongwifi.git package/openwrt-packages/luci-theme-butongwifi
 git clone https://github.com/Repobor/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/openwrt-packages/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld.git package/openwrt-packages/helloworld
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/openwrt-packages/luci-theme-infinityfreedom
 #赋予koolddns权限
 chmod 0755 package/openwrt-packages/luci-app-koolddns/root/etc/init.d/koolddns
