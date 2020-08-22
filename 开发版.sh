9714d9e2894e404a8256c3ee5dde76874f67b85f
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-08-22 18:21:43 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -2,7 +2,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #修改版本号
-sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 
 #添加额外软件包
 git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
