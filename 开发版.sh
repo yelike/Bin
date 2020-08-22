ecc9058d10d2f468f52d2f23d9cd00e8b0a4f9c0
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-08-22 17:23:49 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -10,6 +10,7 @@ svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/ope
 svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/openwrt-packages/chinadns-ng
 #svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/openwrt-packages/tcping
 svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/openwrt-packages/trojan-go
+svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-plus package/openwrt-packages/trojan-plus
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
 git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/openwrt-packages/luci-app-jd-dailybonus
