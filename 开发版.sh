c2e5f55da91baa481d0ac0f222e4d9f7f9ade89e
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-08-30 19:34:53 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -8,7 +8,6 @@
 git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/brook package/openwrt-packages/brook
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng package/openwrt-packages/chinadns-ng
-#svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/openwrt-packages/tcping
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/openwrt-packages/trojan-go
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/openwrt-packages/trojan-plus
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
@@ -22,13 +21,11 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/openwrt
 git clone https://github.com/bin20088/luci-theme-butongwifi.git package/openwrt-packages/luci-theme-butongwifi
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/openwrt-packages/luci-theme-atmaterial
 git clone https://github.com/cnzd/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
-git clone https://github.com/bin20088/luci-app-koolddns.git package/openwrt-packages/luci-app-koolddns
-#git clone https://github.com/fw876/helloworld.git package/openwrt-packages/helloworld
+svn co https://github.com/siropboy/mypackages/trunk/luci-app-koolddns package/openwrt-packages/luci-app-koolddns
+git clone https://github.com/fw876/helloworld.git package/openwrt-packages/helloworld
 git clone https://github.com/tuanqing/install-program package/openwrt-packages/install-program
 svn co https://github.com/0saga0/OpenClash/trunk/luci-app-openclash package/openwrt-packages/luci-app-openclash
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/openwrt-packages/luci-theme-infinityfreedom
-#赋予koolddns权限
-chmod 0755 package/openwrt-packages/luci-app-koolddns/root/etc/init.d/koolddns
-chmod 0755 package/openwrt-packages/luci-app-koolddns/root/usr/share/koolddns/aliddns
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
