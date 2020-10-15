f748a540f182bfc34d16bbef8e5925ab6591d77c
bin20088 <53508344+bin20088@users.noreply.github.com>
2020-10-15 17:45:49 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -5,6 +5,8 @@
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #修改主机名
 sed -i 's/OpenWrt/Bin-Lean/g' package/base-files/files/bin/config_generate
+#关闭自建私有源签名验证
+sed -i '92d' package/system/opkg/Makefile
 
 #添加额外软件包
 git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
